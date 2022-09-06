57ab50a1641f484ad2346c6012269dc28a2b14d1
seanfitz34 <seanwilliamfitzgerald@gmail.com>
2022-09-05 22:35:24 -0700

    worked on html and js

---
+++ assets/JS/script.js
@@ -0,0 +1,5 @@
+// global Variables
+var submit = document.querySelector("#search-btn");
+var citySearch = document.querySelector("section1");
+var uvIndex = document.querySelector("section2");
+var forecast = document.querySelector("section3");
